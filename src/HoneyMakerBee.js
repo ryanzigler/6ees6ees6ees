14951c90b9792cb9cf6f476dca8a0a5ff81d19fd
Ryan Zigler <rpzigler@gmail.com>
2020-07-09 00:47:33 -0500

    Implemented ES6 instatiation pattern on Grub.js, Bee.js, HoneyMakerBee.js, ForagerBee.js, and RetiredForagerBee.js

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
-class HoneyMakerBee {
-  // TODO..
-};
+class HoneyMakerBee extends Bee {
+
+  constructor(color) {
+    super(color);
+    this.age = 10;
+    this.job = 'make honey';
+    this.honeyPot = 0;
+  }
+
+  makeHoney() {
+    return this.honeyPot++;
+  }
+
+  giveHoney() {
+    return this.honeyPot--;
+  }
+}
